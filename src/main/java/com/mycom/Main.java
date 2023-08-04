45d54ac89c88e8147a1481aea385c727118d2055
pmk <peterpmk61@gamil.com>
2023-08-04 17:36:19 +0900

    first commit!!!

---
+++ src/main/java/com/mycom/Main.java
@@ -0,0 +1,7 @@
+package com.mycom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/mycom/myapp/MyappApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mycom.myapp;
+
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+		System.out.printf("contextLoads .........");
+
+	}
+
+}
